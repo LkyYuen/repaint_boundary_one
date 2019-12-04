3801f076e83b82632d58a46f19844522617296a8
LkyYuen <lkylau96@gmail.com>
2019-12-04 18:20:28 +0800

    add text editing

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ import 'dart:math';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 import 'dart:ui';
-
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:gallery_saver/gallery_saver.dart';
+import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:repaint_boundary_one/PermissionsService.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart'; 
 import 'package:flutter_hsvcolor_picker/flutter_hsvcolor_picker.dart' as hsvColor;
 
@@ -20,6 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
@@ -46,6 +49,24 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   Image _image = Image.network("https://cdn.pixabay.com/photo/2019/05/02/16/58/stone-4173970_960_720.jpg");
   var view = "hsvPicker";
 
+  // TEXT VARIABLE START
+  //
+  //
+  final textController = TextEditingController();
+  Offset offset = Offset.zero;
+  var textFontSizeUp = 50.0;
+    double _scale = 1.0;
+  double _previousScale;
+  var yOffset = 400.0;
+  var xOffset = 50.0;
+  var rotation = 0.0;
+  var lastRotation = 0.0;
+  //
+  //
+  // TEXT VARIABLE END
+
+  var editMode;
+  Matrix4 matrix = Matrix4.identity();
 
   // Draw Variable Start
   Color selectedColor = Colors.black;
@@ -65,7 +86,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   ];
   // Draw Variable End
 
-
+  // Color Variable Start
   ValueChanged<HSVColor> onChanged;
   List<Color> linearColor = [
     Color(0xfff32121),
@@ -76,7 +97,8 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     Color(0xfff321f3),
     Color(0xfff32121),
   ];
-  // ColorListener colorListener;
+  var colorSelected;
+  // Color Variable End
 
   // void hueOnChange(double value) => onChanged(color.withHue(value));
 
@@ -100,6 +122,14 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     print(imgFile);
     // await GallerySaver.saveImage(imgFile);
     print("done");
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    textController.dispose();
+    super.dispose();
   }
 
   @override
@@ -167,6 +197,9 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
     Color color = HSVColor.fromAHSV(1.0, percent * 360, 1.0, 1.0).toColor();
     print(color);
+    setState(() {
+      colorSelected = color;
+    });
   }
   // void handleTouch(Offset globalPosition, BuildContext context) {
   //   RenderBox box = context.findRenderObject();
@@ -197,51 +230,253 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: 
-      // hsvPicker
-      Center(
-        child: Container(
-          // color: Colors.red,
-          margin: EdgeInsets.only(top: 100.0),
-          height: 20.0,
-          width: 350.0,
-          child: GestureDetector(
-            // onPanUpdate: (details) {
-            //   if (details.delta.dx > 0)
-            //     print("Dragging in +X direction");
-            //   else
-            //     print("Dragging in -X direction");
 
-            //   if (details.delta.dy > 0)
-            //     print("Dragging in +Y direction");
-            //   else
-            //     print("Dragging in -Y direction");
-            // },
-            onHorizontalDragUpdate: (DragUpdateDetails details) => handleTouch(details.globalPosition, context),
-            // onHorizontalDragUpdate: (DragUpdateDetails update) => _onDragUpdate(context, update),
-            child:
-              DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [
-                      Color(0xfff32121),
-                      Color(0xfff3f321),
-                      Color(0xff21f321),
-                      Color(0xff21f3f3),
-                      Color(0xff2121f3),
-                      Color(0xfff321f3),
-                      Color(0xfff32121),
-                    ],
-                    tileMode: TileMode.clamp
-                  )
+    // SystemChrome.setEnabledSystemUIOverlays([]); // hide status bar
+
+        return Scaffold(
+          body: 
+          // hsvPicker
+          // Center(
+          //   child: Column(
+          //     children: <Widget>[
+          //       Container(
+          //         margin: EdgeInsets.only(top: 100.0),
+          //         color: colorSelected,
+          //         child: Text(colorSelected.toString()),
+          //       ),
+          //        Container(
+          //         // color: Colors.red,
+          //         margin: EdgeInsets.only(top: 100.0),
+          //         height: 20.0,
+          //         width: 350.0,
+          //         child: GestureDetector(
+          //           // onPanUpdate: (details) {
+          //           //   if (details.delta.dx > 0)
+          //           //     print("Dragging in +X direction");
+          //           //   else
+          //           //     print("Dragging in -X direction");
+    
+          //           //   if (details.delta.dy > 0)
+          //           //     print("Dragging in +Y direction");
+          //           //   else
+          //           //     print("Dragging in -Y direction");
+          //           // },
+          //           onHorizontalDragUpdate: (DragUpdateDetails details) => handleTouch(details.globalPosition, context),
+          //           // onHorizontalDragUpdate: (DragUpdateDetails update) => _onDragUpdate(context, update),
+          //           child:
+          //             DecoratedBox(
+          //               decoration: BoxDecoration(
+          //                 gradient: LinearGradient(
+          //                   begin: Alignment.centerLeft,
+          //                   end: Alignment.centerRight,
+          //                   colors: [
+          //                     Color(0xfff32121),
+          //                     Color(0xfff3f321),
+          //                     Color(0xff21f321),
+          //                     Color(0xff21f3f3),
+          //                     Color(0xff2121f3),
+          //                     Color(0xfff321f3),
+          //                     Color(0xfff32121),
+          //                   ],
+          //                   tileMode: TileMode.clamp
+          //                 )
+          //               ),
+          //             ),
+          //         ),
+          //       )
+          //     ],
+          //   ),
+           
+          // )
+    
+          GestureDetector(
+            // onTap: () => FocusScope.of(context).unfocus(),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.blue,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: 300,
+                  maxWidth: 300,
+                  minHeight: 300,
+                  maxHeight: 300,
+                ),
+                child: Stack(
+                  overflow: Overflow.visible,
+                children: <Widget>[
+                  Center(
+                    child: Stack(
+                      children: <Widget>[
+                        _image,
+                        editMode == "textMode" ? 
+                        Container(
+                          child: Positioned(
+                            left: offset.dx,
+                            top: offset.dy,
+                            child: GestureDetector(
+                              onPanUpdate: (details) {
+                                if ((offset.dx + details.delta.dx < MediaQuery.of(context).size.width * 0.8) && (offset.dy + details.delta.dy < MediaQuery.of(context).size.height * 0.8)) {
+                                  setState(() {
+                                    offset = Offset(offset.dx + details.delta.dx, offset.dy + details.delta.dy);
+                                  });
+                                }
+                                print(offset.dx.toString() + " | " + offset.dy.toString());
+                              },
+                              // onScaleStart: (scaleDetails) {
+                              //   _previousScale = _scale;
+                              //   print(' scaleStarts = ${scaleDetails.focalPoint}');
+                              // },
+                              // onScaleUpdate: (scaleUpdates) {
+                              //   lastRotation += scaleUpdates.rotation;
+                              //   var offset = scaleUpdates.focalPoint;
+                              //   xOffset = offset.dx;
+                              //   yOffset = offset.dy;
+
+                              //   setState(() => _scale = _previousScale * scaleUpdates.scale);
+                              // },
+                              // onScaleEnd: (scaleEndDetails) {
+                              //   _previousScale = null;
+                              //   print(' scaleEnds = ${scaleEndDetails.velocity}');
+                              // },
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                height: 150,
+                                child: Center(
+                                        child: 
+                                        TextField(
+                                          maxLines: 10,
+                                          controller: textController,
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                          ),
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: textFontSizeUp,
+                                          ),
+                                        ),
+                                        // Text("You Think You Are Funny But You Are Not",
+                                        //   textAlign: TextAlign.center,
+                                        //   style: TextStyle(
+                                        //     fontWeight: FontWeight.bold,
+                                        //     fontSize: 28.0,
+                                        //     color: Colors.red
+                                        //   )
+                                        // ),.
+                                    ),
+                              )
+                          //   Padding(
+                          //     padding: const EdgeInsets.all(8.0),
+                          //     child: Center(
+                          //       child: 
+                          //       TextField(
+                          //         maxLines: 10,
+                          //         controller: textController,
+                          //         decoration: InputDecoration(
+                          //           border: InputBorder.none,
+                          //         ),
+                          //         style: TextStyle(
+                          //           color: Colors.black,
+                          //           fontSize: textFontSizeUp,
+                          //         ),
+                          //       ),
+                          //       // Text("You Think You Are Funny But You Are Not",
+                          //       //   textAlign: TextAlign.center,
+                          //       //   style: TextStyle(
+                          //       //     fontWeight: FontWeight.bold,
+                          //       //     fontSize: 28.0,
+                          //       //     color: Colors.red
+                          //       //   )
+                          //       // ),
+                          //     ),
+                          //   ),
+                          // )
+                        ),
+                      ),
+                    ) : Container(),
+                    // Draggable(
+                    //   // alignment: Alignment(0.0, 0.0),
+                    //   child: Container(
+                    //     color: Colors.white,
+                    //     child: TextField(
+                    //       controller: textController,
+                    //       // maxLines: 3,
+                    //       decoration: InputDecoration(
+                    //         border: InputBorder.none,
+                    //       ),
+                    //       style: TextStyle(
+                    //         color: Colors.black,
+                    //       ),
+                    //     ),
+                    //   ),
+                    //   feedback: TextField(
+                    //     // controller: textController,
+                    //     // maxLines: 3,
+                    //     decoration: InputDecoration(
+                    //       border: InputBorder.none,
+                    //     ),
+                    //     style: TextStyle(
+                    //       color: Colors.black,
+                    //     ),
+                    //   ),
+                    //   // childWhenDragging: Container(),
+                    // ) : Container(),
+                  ],
                 ),
               ),
+              Row( // TOP RIGHT TOOLBAR
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  IconButton( // DELETE
+                    color: Colors.white,
+                    icon: Icon(Icons.delete_outline),
+                    onPressed: () {
+                      setState(() {
+                        if (selectedMode == SelectedMode.StrokeWidth)
+                          showBottomList = !showBottomList;
+                        selectedMode = SelectedMode.StrokeWidth;
+                      });
+                    }
+                  ),
+                  IconButton( // CROP
+                    color: Colors.white,
+                    icon: Icon(Icons.crop),
+                    onPressed: () {
+                      setState(() {
+                        if (selectedMode == SelectedMode.StrokeWidth)
+                          showBottomList = !showBottomList;
+                        selectedMode = SelectedMode.StrokeWidth;
+                      });
+                    }
+                  ),
+                  IconButton( // WRITE
+                    color: Colors.white,
+                    icon: Icon(Icons.title),
+                    onPressed: () {
+                      setState(() {
+                        editMode = "textMode";
+                      });
+                    }
+                  ),
+                  IconButton( // DRAW
+                    color: Colors.white,
+                    icon: Icon(Icons.create),
+                    onPressed: () {
+                      setState(() {
+                        if (selectedMode == SelectedMode.StrokeWidth)
+                          showBottomList = !showBottomList;
+                        selectedMode = SelectedMode.StrokeWidth;
+                      });
+                    }
+                  ),
+                ],
+              ),
+
+            ],
           ),
-        )
-      )
+          ),
+        ),
+      ),
 
       //  Container(
       //   height: MediaQuery.of(context).size.height,
